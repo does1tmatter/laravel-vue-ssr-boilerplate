079225b84b0f03e5569ce33d50c91e2a3fdb22ec
John Cena <laisvunasmsk@gmail.com>
2024-09-19 23:35:03 +0300

    clean up

--- resources/views/inertia.blade.php
+++ resources/views/inertia.blade.php
@@ -9,25 +9,8 @@
   >
 
   <title>Laravel</title>
-  {{-- Optional - serve css with initial html --}}
-  @if (env('APP_ENV') === 'production')
-    <style>
-      {!! App\Helpers\InlineStyle::fromVite() !!}
-    </style>
-  @endif
 
-  @php
-    $viteAssets = ['resources/js/app.js'];
-
-    // for local dev
-    if (env('APP_ENV') !== 'production') {
-        $viteAssets[] = 'resources/css/app.css';
-    }
-  @endphp
-  {{-- Optional --}}
-
-  {{-- Pass both js and css if not using inlined styles --}}
-  @vite($viteAssets)
+  @vite(['resources/js/app.js', 'resources/css/app.css'])
   @inertiaHead
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home-page', [PagesController::class, 'index'])->name('home');
+Route::get('/', [PagesController::class, 'index'])->name('home');
 Route::get('/new-page', [PagesController::class, 'test'])->name('test');
